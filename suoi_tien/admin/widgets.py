c6b6068121f69a34c42d7bfeb17167b1d9140668
Nguyễn Quốc Huy <nguyenquochuyc7@gmail.com>
2026-06-25 22:53:42 +0700

    fix(menu): align data format with build_menu_tree, clean titles, show parent labels

--- suoi_tien/admin/widgets.py
+++ suoi_tien/admin/widgets.py
@@ -2,17 +2,28 @@ import json
 from django import forms
 from django.template.loader import render_to_string
 from ..models import HalinkPost
+from ..utils import clean_lang
 
 class MenuBuilderWidget(forms.Textarea):
     template_name = 'admin/suoi_tien/widgets/menu_builder.html'
 
     def get_context(self, name, value, attrs):
         context = super().get_context(name, value, attrs)
-        
-        # Lấy danh sách các trang, chuyên mục để hiển thị ở cột trái
-        pages = list(HalinkPost.objects.filter(post_type='page', ticlock='0').values('Id', 'title_vn'))
-        post_cats = list(HalinkPost.objects.filter(post_type='postcat', ticlock='0').values('Id', 'title_vn'))
-        product_cats = list(HalinkPost.objects.filter(post_type='productcat', ticlock='0').values('Id', 'title_vn'))
+
+        # Lấy danh sách các trang, chuyên mục để hiển thị ở cột trái - làm sạch tiêu đề
+        # (bỏ thẻ [[[:vi]]]...[[[:end_vi]]]) để không hiện chữ thẻ thô ngoài giao diện.
+        pages = [
+            {'Id': p['Id'], 'title_vn': clean_lang(p['title_vn'])}
+            for p in HalinkPost.objects.filter(post_type='page', ticlock='0').values('Id', 'title_vn')
+        ]
+        post_cats = [
+            {'Id': p['Id'], 'title_vn': clean_lang(p['title_vn'])}
+            for p in HalinkPost.objects.filter(post_type='postcat', ticlock='0').values('Id', 'title_vn')
+        ]
+        product_cats = [
+            {'Id': p['Id'], 'title_vn': clean_lang(p['title_vn'])}
+            for p in HalinkPost.objects.filter(post_type='productcat', ticlock='0').values('Id', 'title_vn')
+        ]
         
         # Load toàn bộ bài viết, chuyên mục vào map để luôn dịch được ID -> Tên
         title_map = {}
